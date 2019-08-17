cf0ee1bd4c047dc07023fd7f65f43fc3da0ce70b
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-17 16:53:36 +0800

    found typo on identifier and issue on encode, but fetch is still not working

--- JournalCoreData/Model Controllers/EntryController.swift
+++ JournalCoreData/Model Controllers/EntryController.swift
@@ -140,7 +140,7 @@ class EntryController {
         guard let identifier = identifier else { return nil }
         
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "identfier == %@", identifier)
+        fetchRequest.predicate = NSPredicate(format: "identifier == %@", identifier)
         
         var result: Entry? = nil
         do {
